45806a2b5a5e21eefc0265ceb1cd4531aad24a9e
wangsiyuan233 <380191957@qq.com>
2018-05-27 19:35:45 +0800

    更新css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+ import React, { Component } from 'react';
+ import 'normalize.css'
+ import './reset.css'
+ import './App.css';
+ import TodoInput from './TodoInput'
+ import TodoItem from './TodoItem'
+
+ class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      newTodo: 'test',
+      todoList: [
+        {id:1, title:'第一个待办'},
+        {id:2, title:'第二个待办'},
+      ]
+    }
   }
-}
+   render() {
+    let todos = this.state.todoList.map((item,index)=>{
+      return (
+        <li>
+          <TodoItem todo={item} />
+        </li>
+      )
+    })
+     return (
+       <div className="App">
+          <h1>我的待办</h1>
+          <div className="inputWrapper">
+	          <TodoInput content={this.state.newTodo} />
+          </div>
+        <ol>
+          {todos}
+        </ol>
+      </div>
+     )
+   }
+ }
 
 export default App;
