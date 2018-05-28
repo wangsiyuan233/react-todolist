49ada16dc61f5f998684d32225c75de67ed07af1
wangsiyuan233 <380191957@qq.com>
2018-05-28 10:20:54 +0800

    传入index.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,79 @@
+import React, { Component } from 'react';
+import 'normalize.css'
+import './reset.css'
+import './App.css';
+import TodoInput from './TodoInput'
+import TodoItem from './TodoItem'
 
- import React, { Component } from 'react';
- import 'normalize.css'
- import './reset.css'
- import './App.css';
- import TodoInput from './TodoInput'
- import TodoItem from './TodoItem'
-
- class App extends Component {
+class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      newTodo: 'test',
+      newTodo: '',
       todoList: [
-        {id:1, title:'第一个待办'},
-        {id:2, title:'第二个待办'},
       ]
     }
   }
-   render() {
-    let todos = this.state.todoList.map((item,index)=>{
-      return (
-        <li>
-          <TodoItem todo={item} />
+  render() {
+
+    let todos = this.state.todoList
+      .filter((item)=> !item.deleted)
+      .map((item,index)=>{
+      return ( // 为什么这里要加个括号？这是动手题3 🐸
+        <li key={index} >
+          <TodoItem todo={item} onToggle={this.toggle.bind(this)}
+            onDelete={this.delete.bind(this)}/>
         </li>
       )
     })
-     return (
-       <div className="App">
-          <h1>我的待办</h1>
-          <div className="inputWrapper">
-	          <TodoInput content={this.state.newTodo} />
-          </div>
-        <ol>
+
+    return (
+      <div className="App">
+        <h1>我的待办</h1>
+        <div className="inputWrapper">
+          <TodoInput content={this.state.newTodo}
+            onChange={this.changeTitle.bind(this)}
+            onSubmit={this.addTodo.bind(this)} />
+        </div>
+        <ol  className="todoList">
           {todos}
         </ol>
       </div>
-     )
-   }
- }
+    )
+  }
+  toggle(e, todo){
+    todo.status = todo.status === 'completed' ? '' : 'completed'
+    this.setState(this.state)
+  }
+  changeTitle(event){
+    this.setState({
+      newTodo: event.target.value,
+      todoList: this.state.todoList
+    })
+  }
+  addTodo(event){
+    this.state.todoList.push({
+      id: idMaker(),
+      title: event.target.value,
+      status: null,
+      deleted: false
+    })
+    this.setState({
+      newTodo: '',
+      todoList: this.state.todoList
+    })
+  }
+  delete(event, todo){
+    todo.deleted = true
+    this.setState(this.state)
+  }
+}
 
 export default App;
+
+let id = 0
+
+function idMaker(){
+  id += 1
+  return id
+}
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <App />,
   document.getElementById('root')
 );
